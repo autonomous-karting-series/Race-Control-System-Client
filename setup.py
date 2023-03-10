6bf7b889fc38cd30b982da233636c0fd62256f2b
Alan Chung Ma <alanchunggt@gmail.com>
2023-03-10 14:57:25 -0500

    add pre-commit-config

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+         ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools', 'paho-mqtt'],
@@ -19,8 +19,7 @@ setup(
     license='GNU General Public License 3.0',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [
-            "aks_rcs_client_node = aks_rcs_client.aks_rcs_client:main"
-        ],
+        'console_scripts':
+        ["aks_rcs_client_node = aks_rcs_client.aks_rcs_client:main"],
     },
 )
