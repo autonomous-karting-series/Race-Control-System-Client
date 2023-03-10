85bd3aefb54ee3d065ec4b364d86cb52bce63627
Alan Chung Ma <alanchunggt@gmail.com>
2023-03-10 15:28:23 -0500

    clean up

--- aks_rcs_client/aks_rcs_client.py
+++ aks_rcs_client/aks_rcs_client.py
@@ -12,38 +12,32 @@ class AKSRCSClient(Node):
         super().__init__('aks_rcs_client')
         self.declare_parameters(
             namespace='',
-            parameters=[
-                ('mqtt_host', '127.0.0.1'),  # Address to mqtt broker
-                ('mqtt_port', 1883),  # Port to mqtt broker
-                ('team_id',
-                 'team_id'),  # Used for client name and mqtt kart state topic
-                ('mqtt_track_state_topic', 'track_state'),
-                ('mqtt_kart_state_topic', 'kart_state'),
+            parameters=[('team_id', 'TEAM_ID',
+                         'Team ID, unique identifier for each team'),
+                        ('mqtt_host', '127.0.0.1', 'Address of mqtt broker'),
+                        ('mqtt_port', 1883, 'Port of mqtt broker'),
+                        ('mqtt_track_state_topic', 'track_state'),
+                        ('mqtt_kart_state_topic', 'kart_state'),
+                        ('ros_track_state_topic', 'track_state',
+                         'ROS topic to publish track state to'),
+                        ('ros_kart_state_topic', 'kart_state',
+                         'ROS topic to subscribe to for kart state')])
 
-                # Topic to publish track state within ROS
-                ('ros_track_state_topic', 'track_state'),
-
-                # Topic to get kart state within ROS
-                ('ros_kart_state_topic', 'kart_state')
-            ])
-
-        # Set Parameter values to variables
-        client_name = self.get_parameter('team_id').value
+        # Get params
+        team_id = self.get_parameter('team_id').value
         mqtt_host = self.get_parameter('mqtt_host').value
         mqtt_port = self.get_parameter('mqtt_port').value
         mqtt_track_topic = self.get_parameter('mqtt_track_state_topic').value
-
-        # String not parameter object like previous variables
-        self.mqtt_kart_topic = str(
-            self.get_parameter('mqtt_kart_state_topic').value) + '/' + str(
-                self.get_parameter('team_id').value)
+        mqtt_kart_topic = self.get_parameter('mqtt_kart_state_topic').value
         ros_track_topic = self.get_parameter('ros_track_state_topic').value
         ros_kart_topic = self.get_parameter('ros_kart_state_topic').value
+
+        # full mqtt topic to publish kart state
+        self.mqtt_kart_topic = f"{mqtt_kart_topic}/{team_id}"
 
         # Initialize ros2 publisher and subscriber
         self.race_state_pub = self.create_publisher(String, ros_track_topic,
                                                     10)
-        # Change data type if needed. not sure what data will be sent from topic.
         self.race_kart_sub = self.create_subscription(String, ros_kart_topic,
                                                       self.listener_callback,
                                                       10)
@@ -60,7 +54,7 @@ class AKSRCSClient(Node):
             client.disconnect()
 
         # Initialize mqtt publishers and subscribers
-        self.mqttclient = mqtt.Client(client_name)
+        self.mqttclient = mqtt.Client(team_id)
         self.mqttclient.on_disconnect = on_disconnect
         self.mqttclient.on_message = on_message
         self.mqttclient.connect(mqtt_host, port=mqtt_port)
